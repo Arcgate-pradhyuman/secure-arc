1cce07165a14f2b72cec0a57c60a92fb298e4323
pradhyuman <pradhyuman@arcgate.com>
2023-08-23 10:38:53 +0530

    arc-encrypt package

---
+++ packages/arc-encrypt/jwt.js
@@ -0,0 +1,23 @@
+// jwt.js
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+function generateToken(payload, expiresIn = 3600) {
+    return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+function verifyToken(token) {
+    try {
+        return jwt.verify(token, SECRET_KEY);
+    } catch (error) {
+        console.error('Access token verification failed:', error.message);
+        return null;
+    }
+}
+
+module.exports = {
+    generateToken,
+    verifyToken
+};
