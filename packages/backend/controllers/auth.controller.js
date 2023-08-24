327ac2d4dc84ef6e5b5edc2cbbc4ab906fb75311
Arcgate-ravindra <137500358+Arcgate-ravindra@users.noreply.github.com>
2023-08-24 15:01:01 +0530

    Ravi (#2)
    merge inside develop

---
+++ packages/backend/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const db = require("../database/db");
+const {
+  jwt: { generateToken, verifyToken },
+} = require("arc-encrypt");
+
+const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await db.collection("users").findOne({ username: username });
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+    const accessToken = generateToken({ userId: user._id });
+    const refreshToken = generateToken({ userId: user._id }, 604800); // 1 week for refresh token
+    return res.status(200).json({ accessToken, refreshToken });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const token = (req, res, next) => {
+  try {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+      return res.status(403).json({ message: "Refresh token is required" });
+    }
+
+    const decodedToken = verifyToken(refreshToken);
+
+    if (!decodedToken) {
+      return res.status(403).json({ message: "Invalid refresh token" });
+    }
+
+    const newAccessToken = generateToken({ userId: decodedToken.userId });
+    return res.status(200).json({ accessToken: newAccessToken });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { login, token };
--- packages/backend/routes/users.js
+++ packages/backend/routes/users.js
@@ -1,9 +1,15 @@
-var express = require("express");
-var router = express.Router();
+const router = require("express").Router();
+const {createUser,getUser} = require('../controllers/user.controller');
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+
+// create user
+router.post('/create', createUser);
+//get user
+router.get('/get/:userId', getUser);
 
 module.exports = router;
