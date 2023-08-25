c22c4e3e7b8b946a7e50cd93a7dfec752f5ca993
pradhyuman <pradhyuman@arcgate.com>
2023-08-25 17:56:53 +0530

    mongo dump script

--- scripts/docker/dump.sh
+++ scripts/docker/dump.sh
@@ -1,3 +1,3 @@
-sudo docker exec -it mongodb_container mongodump -o /dump
-sudo docker cp mongodb_container:/dump ./dump
-echo "Dump completed!"
+sudo docker exec -it mongodb_container mongodump -d secure -o /dump
+sudo docker cp mongodb_container:/dump/secure ./dump
+echo "Dump of 'secure' completed!"
