7e58e5acba95c023b9b070f5bd1d5435182caa2c
Arcgate-pradhyuman <142872879+Arcgate-pradhyuman@users.noreply.github.com>
2023-09-01 15:37:08 +0530

    merge with develop husky and eslint and new pacakges setup (#11)
    * setup jest and hash functions
    
    * mongo docker setup
    
    * mongo docker cammands
    
    * mongo dump script
    
    * restore sh
    
    * mongo backup
    
    * mongodb docker setup
    
    * chnages in docker compose
    
    * setup cypress
    
    * editor config and github setup
    
    * added pm2 config file
    
    * added swagger
    
    * arc thirdparty and arc utils
    
    * arc-third party
    
    * utilities package
    
    * changed enviroment string
    
    * fix jest config file name
    
    * added husky and changes in eslint

---
+++ scripts/pm2/pm2.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "secure-arc-backend",
+      cwd: "./packages/backend",
+      script: "yarn",
+      args: "start",
+      watch: true,
+      env: {
+        NODE_ENV: "development"
+      }
+    },
+    {
+      name: "secure-arc-frontend",
+      cwd: "./packages/frontend",
+      script: "yarn",
+      args: "dev",
+      watch: true,
+      env: {
+        NODE_ENV: "development"
+      }
+    }
+  ]
+};
